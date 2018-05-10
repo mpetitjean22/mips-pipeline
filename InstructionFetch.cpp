e55084060e10e641e5dde72f2ddf77f70b929443
slothMan <marie.k.petitjean@gmail.com>
2018-05-10 02:01:46 +0000

    idk i guess this would wokr?

---
+++ InstructionFetch.cpp
@@ -0,0 +1,14 @@
+static bool PCSrc;
+
+void run(){
+    /* get instruction that is at PC */
+
+    /* PCSrc = EX/MEM(Zero & Branch) */
+        /* if PCSrc == 0 --> No changes to the PC value */
+        /* else PCsrc == 1 --> PC = EX/MEM BranchAddress */
+
+    /* update IF/ID with the new instruction */
+    /* update IF/ID with the  PC */
+
+    /* PC = PC + 4 */
+}
