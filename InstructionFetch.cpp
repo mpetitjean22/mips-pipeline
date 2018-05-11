817c934529d6e2f96892ad5e7b473aa9179d609c
slothMan <marie.k.petitjean@gmail.com>
2018-05-11 21:57:14 +0000

    i forgot IF in the last commit oops

--- InstructionFetch.cpp
+++ InstructionFetch.cpp
@@ -1,14 +1,10 @@
-static bool PCSrc;
-
-void run(){
-    /* get instruction that is at PC */
-
-    /* PCSrc = EX/MEM(Zero & Branch) */
-        /* if PCSrc == 0 --> No changes to the PC value */
-        /* else PCsrc == 1 --> PC = EX/MEM BranchAddress */
-
-    /* update IF/ID with the new instruction */
-    /* update IF/ID with the  PC */
-
-    /* PC = PC + 4 */
+static uint32_t PCsrc;
+static uint32_t Instruction;
+static uint32_t PC;
+void runInstructionFetch(){
+    Instuction = getSomething(PC);
+    PC = PC + 4;
+    
+    IFtoID.SetInstruction(Instruction);
+    IFtoID.SetPC(PC);
 }
